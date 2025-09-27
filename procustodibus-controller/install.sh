b126fcd1e610ffcfecf94eb928eda2ca44cdcf9b
Roman Akinfeev <akinfold@gmail.com>
2025-09-27 15:36:46 +0300

    add check for traefik3

--- procustodibus-controller/install.sh
+++ procustodibus-controller/install.sh
@@ -12,6 +12,12 @@ if ! type docker > /dev/null 2>&1; then
     exit 1
 fi
 
+# Check traefik 3 already installed
+if [[ "$( sudo docker container inspect -f '{{.State.Status}}' "traefik" 2>&1 )" != "running" ]]; then
+    echo "Please setup traefik 3 with ../traefik3/install.sh script before run this setup."
+    exit 1
+fi
+
 # Source: https://docs.procustodibus.com/guide/onpremises/install/#docker
 # Folder we will use to store all Pro Custodibus related configurations
 sudo -u $PROJECT_USER_NAME mkdir -p "$DOCKER_APPDATA_PATH/procustodibus-controller"
