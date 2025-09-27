f4780c8ebfd24cfe0dffe2b0256438f1df854494
Roman Akinfeev <akinfold@gmail.com>
2025-09-27 15:31:01 +0300

    init pro custodibus setup development

---
+++ procustodibus-controller/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit immediately if a pipeline returns a non-zero status.
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#The-Set-Builtin
+set -e 
+
+. "../env.sh"
+
+# Check docker already installed
+if ! type docker > /dev/null 2>&1; then
+    echo "Please setup docker with ../docker/install.sh script before run this setup."
+    exit 1
+fi
+
+# Source: https://docs.procustodibus.com/guide/onpremises/install/#docker
+# Folder we will use to store all Pro Custodibus related configurations
+sudo -u $PROJECT_USER_NAME mkdir -p "$DOCKER_APPDATA_PATH/procustodibus-controller"
+cd "$DOCKER_APPDATA_PATH/procustodibus-controller"
+sudo -u $PROJECT_USER_NAME curl -L -c /tmp/srht.cookies -b /tmp/srht.cookies https://git.sr.ht/~arx10/procustodibus-api/blob/main/ops/install/generate-docker-compose.sh | bash -s ce
+
+# Change ports because we use Traefik to terminate TLS.
