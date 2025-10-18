aa26e232eb7c3d129087e868b21acadeebb72313
Roman Akinfeev <akinfold@gmail.com>
2025-10-18 03:44:47 +0300

    move secrets to env

--- procustodibus-controller/install.sh
+++ procustodibus-controller/install.sh
@@ -30,40 +30,19 @@ fi
 #
 PROCUSTODIBUS_DB_USER_PASSWORD=$(pwgen -1BC 16 1)
 cat initdb/init.sql | sed "s/{{ PROCUSTODIBUS_USER_PASSWORD }}/$PROCUSTODIBUS_DB_USER_PASSWORD/" | sudo docker exec -i postgresql psql -h localhost -U postgres -f-
-PROCUSTODIBUS_DB_USER_PASSWORD_FILE="$SECRETS_PATH/PROCUSTODIBUS_DB_USER_PASSWORD"
-# Save password to secrets.
-echo "Write postgres new password \"$PROCUSTODIBUS_DB_USER_PASSWORD\" to $PROCUSTODIBUS_DB_USER_PASSWORD_FILE."
-echo "$PROCUSTODIBUS_DB_USER_PASSWORD" | sudo tee $PROCUSTODIBUS_DB_USER_PASSWORD_FILE
-sudo chown root:root $PROCUSTODIBUS_DB_USER_PASSWORD_FILE
-# Write down path to password file to main docker environment. So we can use it in docker-compose.yml
-sudo sed -i "/^PROCUSTODIBUS_DB_USER_PASSWORD_FILE=.*/d" $DOCKER_ENV_FILE
-echo "PROCUSTODIBUS_DB_USER_PASSWORD_FILE=\"$PROCUSTODIBUS_DB_USER_PASSWORD_FILE\"" | sudo tee -a $DOCKER_ENV_FILE
+echo "New postgresql procustodibus_user password is \"$PROCUSTODIBUS_DB_USER_PASSWORD\"."
 
 #
 # Generate application-level encryption key 1
 #
 PROCUSTODIBUS_DB_ALEK_1="value:$( openssl rand -base64 32 )"
-PROCUSTODIBUS_DB_ALEK_1_FILE="$SECRETS_PATH/PROCUSTODIBUS_DB_ALEK_1"
-# Save password to secrets.
-echo "Write new application-level encryption key 1 \"$PROCUSTODIBUS_DB_ALEK_1\" to $PROCUSTODIBUS_DB_ALEK_1_FILE."
-echo "$PROCUSTODIBUS_DB_ALEK_1" | sudo tee $PROCUSTODIBUS_DB_ALEK_1_FILE
-sudo chown root:root $PROCUSTODIBUS_DB_ALEK_1_FILE
-# Write down path to password file to main docker environment. So we can use it in docker-compose.yml
-sudo sed -i "/^PROCUSTODIBUS_DB_ALEK_1_FILE=.*/d" $DOCKER_ENV_FILE
-echo "PROCUSTODIBUS_DB_ALEK_1_FILE=\"$PROCUSTODIBUS_DB_ALEK_1_FILE\"" | sudo tee -a $DOCKER_ENV_FILE
+echo "New application-level encryption key 1 is \"$PROCUSTODIBUS_DB_ALEK_1\"."
 
 #
 # Generate signup key required to create new organization at $APP_URL/signup.
 #
 PROCUSTODIBUS_SIGNUP_KEY=$( openssl rand -base64 12 )
-PROCUSTODIBUS_SIGNUP_KEY_FILE="$SECRETS_PATH/PROCUSTODIBUS_SIGNUP_KEY"
-# Save password to secrets.
-echo "Write new signup key \"$PROCUSTODIBUS_SIGNUP_KEY\" to $PROCUSTODIBUS_SIGNUP_KEY_FILE."
-echo "$PROCUSTODIBUS_SIGNUP_KEY" | sudo tee $PROCUSTODIBUS_SIGNUP_KEY_FILE
-sudo chown root:root $PROCUSTODIBUS_SIGNUP_KEY_FILE
-# Write down path to password file to main docker environment. So we can use it in docker-compose.yml
-sudo sed -i "/^PROCUSTODIBUS_SIGNUP_KEY_FILE=.*/d" $DOCKER_ENV_FILE
-echo "PROCUSTODIBUS_SIGNUP_KEY_FILE=\"$PROCUSTODIBUS_SIGNUP_KEY_FILE\"" | sudo tee -a $DOCKER_ENV_FILE
+echo "New signup key is \"$PROCUSTODIBUS_SIGNUP_KEY\"."
 
 # 
 # Create folder structure for procustodibus and copy files.
@@ -75,11 +54,9 @@ sudo -u $PROJECT_USER_NAME mkdir -p "$DOCKER_APPDATA_PATH/procustodibus-controll
 sudo -u $PROJECT_USER_NAME mkdir -p "$DOCKER_APPDATA_PATH/procustodibus-controller/nginx"
 sudo -u $PROJECT_USER_NAME mkdir -p "$DOCKER_APPDATA_PATH/procustodibus-controller/work"
 
-sudo -u $PROJECT_USER_NAME cp "./api.env" "$DOCKER_COMPOSE_PATH/procustodibus-controller/api.env"
+cat "./api.env" | sed "s/{{ PROCUSTODIBUS_DB_ALEK_1 }}/$PROCUSTODIBUS_DB_ALEK_1/" | sed "s/{{ PROCUSTODIBUS_DB_USER_PASSWORD }}/$PROCUSTODIBUS_DB_USER_PASSWORD/" | sed "s/{{ PROCUSTODIBUS_SIGNUP_KEY }}/$PROCUSTODIBUS_SIGNUP_KEY/" | sudo -u $PROJECT_USER_NAME tee "$DOCKER_COMPOSE_PATH/procustodibus-controller/api.env"
 sudo -u $PROJECT_USER_NAME cp "./app.env" "$DOCKER_COMPOSE_PATH/procustodibus-controller/app.env"
 sudo -u $PROJECT_USER_NAME cp "./docker-compose.yml" "$DOCKER_COMPOSE_PATH/procustodibus-controller/docker-compose.yml"
-sudo -u $PROJECT_USER_NAME cp "./entrypoint.sh" "$DOCKER_APPDATA_PATH/procustodibus-controller/entrypoint.sh"
-sudo -u $PROJECT_USER_NAME chmod +x "$DOCKER_APPDATA_PATH/procustodibus-controller/entrypoint.sh"
 
 
 TRAEFIK_HOSTNAME=$( sudo grep 'TRAEFIK_HOSTNAME' "$DOCKER_ROOT_PATH/.env" | cut -d= -f2 | sed -e 's:#.*$::g' -e 's/^"//' -e 's/"$//' )
